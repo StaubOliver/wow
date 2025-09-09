5fd63a73a824b9b18511d4c39e396938f2da0e27
Olivier Staub <staub.olivier@gmail.com>
2025-09-09 11:29:19 +0200

    Update move_item.py

--- move_item.py
+++ move_item.py
@@ -83,7 +83,7 @@ def  read_csv(path, deli=";"):
 def ordinal(n):
     return str(n)+("th" if 4<=n%100<=20 else {1:"st",2:"nd",3:"rd"}.get(n%10, "th"))
 
-def dtStylish(dt,f):
+def dtStylish(dt):
     f= '%B {th}, %Y'
     return dt.strftime(f).replace("{th}", ordinal(dt.day))
 
